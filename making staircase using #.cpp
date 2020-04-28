13bb68697b49c1a0ba0f853a1ca8fe04a98d2fcb
kunal kumar <1901010019@krmangalam.ac.in>
2020-04-28 19:54:31 +0530

    Add files via upload

---
+++ making staircase using #.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void staircase( int n);
+int main()
+{
+	int i, a[50], n;
+	cin>>n;
+	staircase(int n);
+	return 0;
+}
+void staircase(int a[50], int n)
+{    
+    int i;
+	for(i=0; i<n; i++)
+	cout<<"#"<<endl;
+	
+}
